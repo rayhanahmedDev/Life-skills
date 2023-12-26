3f1202aee277828883e855f5686433853abd14ce
rayhan9802 <mdrayhaanahmed@gmail.com>
2023-12-26 18:08:19 +0600

    make slider from review section

---
+++ exam.js
@@ -0,0 +1,44 @@
+const carousel = document.querySelector('.carousel')
+const arrowBtns = document.querySelectorAll('.wrapper i')
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children]
+
+let isDragging = false, startX, startScrollLeft;
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth)
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML)
+})
+
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML)
+})
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+})
+
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add('dragging')
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove('dragging')
+}
+
+carousel.addEventListener("mousedown", dragStart)
+carousel.addEventListener("mousemove", dragging)
+document.addEventListener("mouseup", dragStop)
